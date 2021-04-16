8570dd0dab357fbeb932ec44e8442fe17cbfd2ef
andrewski619 <andreweuyoque@yahoo.com>
2021-04-15 21:09:22 -0700

    Using the environment variable setup for flask since it was not working before with python

--- start.py
+++ start.py
@@ -32,15 +32,13 @@ class Start:
         """
         dir_path = os.path.dirname(os.path.realpath(__file__))
         os.environ["FLASK_APP"] = os.path.join(str(dir_path), 'node_server.py')
-        current_env = os.environ.copy()
-        return current_env
 
     def start_flask(self):
         """
         Starts the required ports using python flask
         """
         for counter, port in enumerate(Globals.FLASK_PORTS):
-            flask_command = ['python', '-m', 'flask', 'run', '--port', str(port), '&']
+            flask_command = ['flask', 'run', '--port', str(port), '&']
             print('Executing flask port # ' + Globals.YELLOW + str(port) + Globals.NOCOLOR + ' with command: ' + Globals.YELLOW + ' '.join(flask_command) + Globals.NOCOLOR)
             sys.stdout.flush()
             self.execute_process(command=flask_command)
@@ -70,7 +68,7 @@ class Start:
         """
         Opens a subprocess to execute the command with updated env
         """
-        proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, env=os.environ)
         Globals.OPENED_PROCESSES.append(proc.pid)
         time.sleep(2)
 
@@ -84,7 +82,7 @@ def main():
         return 1
 
     start = Start()
-    #updated_env = start.setup_flask_environment()
+    start.setup_flask_environment()
     start.start_flask()
     start.start_web_app()
 
